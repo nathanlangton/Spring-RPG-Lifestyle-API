45bf8a2f619dac776e84af30b2eec6bc404bc71a
skelxtondev <dorkhrs@gmail.com>
2024-02-07 22:15:31 +0000

    Init

---
+++ src/test/java/com/nathan/langton/RPGLIfestyleAPI/RpglIfestyleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.langton.RPGLIfestyleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpglIfestyleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
